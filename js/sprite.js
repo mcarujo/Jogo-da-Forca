653277b2fb439d8c73892742892c9d81f1df6428
Marco Carujo <mamcarujo@gmail.com>
2019-04-28 17:03:38 -0300

    sprite js para controle da imagem da forca

---
+++ js/sprite.js
@@ -0,0 +1,47 @@
+function createSprite(element) {
+  var obj = {
+    frames: [
+      "frame1",
+      "frame2",
+      "frame3",
+      "frame4",
+      "frame5",
+      "frame6",
+      "frame7",
+      "frame8",
+      "frame9"
+    ],
+    element: $(element),
+    currentFrame: 0,
+    lastFrame: 8,
+    hasFrame: function() {
+      if (this.currentFrame == this.lastFrame) {
+        return false;
+      } else {
+        return true;
+      }
+    },
+    resetFrame: function() {
+      this.element
+        .removeClass(this.frames[this.currentFrame])
+        .addClass(this.frames[0]);
+      this.currentFrame = 0;
+    },
+    moveFrame: function(from, to) {
+      this.element.removeClass(from).addClass(to);
+    },
+    nextFrame: function() {
+      // console.log(this.currentFrame, this.lastFrame, this.frames);
+      // defense
+      if (this.hasFrame()) {
+        this.resetFrame();
+      } else {
+        this.moveFrame(
+          this.frames[this.currentFrame],
+          this.frames[++this.currentFrame]
+        );
+      }
+    }
+  };
+  return obj;
+}
